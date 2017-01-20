9f6529439f42bc4988fbab50fe043d5e8395eba5
tylertaylor <vikingtyty@gmail.com>
2017-01-19 23:11:31 -0600

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -1,8 +1,8 @@
 describe('Directive Test', function() {
-	browser.get('http://localhost:8080');
+	browser.get('http://localhost:8080'); // grab the page
 
-	var counter = element(by.css('.counter'));
-	var count = element(by.css('.counter__count'));
+	var counter = element(by.css('.counter')); // grab the counter directive
+	var count = element(by.css('.counter__count')); // grab the current count
 
 	it('should have an initial 0 count', function () {
 		expect(count.getInnerHtml()).toEqual('Current count: 0');
